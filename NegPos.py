a7b3a952d2b95d010036322c3016d91a59a0d93f
Eliya Lotfi <mr.eliyalotfi@gmail.com>
2022-08-15 09:45:15 +0430

    Defining a function to detect the lips' poses(Neg or Pos)

--- NegPos.py
+++ NegPos.py
@@ -13,3 +13,15 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.decomposition import PCA
 from joblib import dump
 from mtcnn import MTCNN
+
+detector = MTCNN()
+
+def detect_lips(img):
+    rgb_img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)         
+    out = detector.detect_faces(rgb_img)[0]
+    x,y,w,h=out["box"]
+    nose=out["keypoints"]['nose'][1]
+    mouth_left=out["keypoints"]['mouth_left'][0]
+    mouth_right=out["keypoints"]['mouth_right'][0]
+    img = img[nose:y+h,mouth_left:mouth_right]
+    return img
