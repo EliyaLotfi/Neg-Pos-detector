829b80643ccc3e074ca407594706646a746f7a7e
Eliya Lotfi <mr.eliyalotfi@gmail.com>
2022-08-15 09:43:15 +0430

    import necessary modules

--- NegPos.py
+++ NegPos.py
@@ -1,2 +1,15 @@
 import os
 os.environ["TF_CPP_MIN_LOG_LEVEL"]= "3"
+
+import cv2
+from sklearn.model_selection import train_test_split
+import glob
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+from joblib import dump,load
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression,SGDClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.decomposition import PCA
+from joblib import dump
+from mtcnn import MTCNN
